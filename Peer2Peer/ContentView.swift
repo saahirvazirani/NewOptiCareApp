fe7000a4fd9dc04099a862667ab1426741a8e927
saahirvazirani <rhinohogsv@gmail.com>
2023-10-19 20:16:26 -0400

    Initial Commit

---
+++ Peer2Peer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Peer2Peer
+//
+//  Created by Saahir Vazirani on 10/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Peer2Peer/Peer2PeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peer2PeerApp.swift
+//  Peer2Peer
+//
+//  Created by Saahir Vazirani on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Peer2PeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
